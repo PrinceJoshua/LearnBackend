c07334426a2692e8a421f4af5a0fa62e0ab0c67d
Joshua <atherleyjoshua@gmail.com>
2021-11-14 04:06:10 -0400

    database conected

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+//checking if in production environment 
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+
+
+}
+
+
 const express = require('express') //
 const app = express()
 const expressLayouts = require('express-layouts')
@@ -16,6 +24,17 @@ app.use(express.static('public'))
 
 //using the //the router or controller file
 app.use('/', indexRouter)
+
+//import mongo db 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true})
+
+
+const db = mongoose.connection
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('Database Connected'))
+
+
 
 app.listen(process.env.PORT || 3000)
 
